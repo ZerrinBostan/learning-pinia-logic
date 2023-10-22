26b3b7d89d9fb7042a179835d661578dec6facaa
zerrinBostan <zerrinbstn@gmail.com>
2023-10-22 15:28:05 +0300

    Created a Pinia store named 'todoListStore'. Added state for managing a list of TODO items. Implemented actions for adding, removing, and updating TODO items

---
+++ src/stores/todoListStore.js
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+
+export const useTodoListStore = defineStore("todoListStore", {
+  /*
+  Basic Logic
+  ┌─────────────────────────────────────────────────────────────────────────────┐
+  │     // state                                                                │
+  │     // getters                                                              │
+  │     // actions                                                              │
+  └─────────────────────────────────────────────────────────────────────────────┘
+ */
+
+  state: () => ({
+    list: [],
+    id: 0,
+  }),
+  actions: {
+    addTodo(item) {
+      this.list.push({ description: item, id: this.id++, completed: false });
+    },
+    removeTodo(itemId) {
+      this.list = this.list.filter((listItem) => {
+        return listItem.id !== itemId;
+      });
+    },
+    updateTodo(itemId, updatedItem) {
+      const todo = this.list.find((item) => item.id === itemId);
+
+      if (todo) {
+        todo.description = updatedItem;
+      }
+    },
+    // filterItemsById(item) {
+
+    // }
+  },
+});
