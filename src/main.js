301c8d210909e536ab8100d7564e555f11c69595
zerrinBostan <zerrinbstn@gmail.com>
2023-10-22 15:26:57 +0300

    Created Pinia store for state management. Integrated Pinia with the app. Mounted the Vue 3 app

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './styles/globals.css';
+
+import App from './App.vue'
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
